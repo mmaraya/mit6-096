8232fbe56985eeec6a5158f85a294daab1a6f805
mmaraya <mike.maraya@gmail.com>
2013-12-23 21:58:59 -0500

    Completed lab3, exercise 6

---
+++ src/lab3/piglatin.cpp
@@ -0,0 +1,54 @@
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2013 Michael Maraya
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+#include "piglatin.h"
+
+// In this section you will write a program that turns a given English word into Pig Latin
+string pigLatinify(const string str)
+{
+    string result = string(str);
+    
+    // word begins with consonant
+    if (VOWELS.find_first_of(str.front()) == string::npos) {
+        result.erase(0, 1);
+        result.push_back(str.front());
+        if (str.front() == 'q') {
+            result.erase(0, 1);
+            result.push_back('u');
+        }
+        result.append("ay");
+    } else {
+        result.append("way");
+    }
+    
+    return result;
+}
+
+int main(const int argc, const char *argv[])
+{
+    string words[] = {"alpha", "bravo", "quebec"};
+    for (int i = 0; i < 3; i++) {
+        cout << words[i] << ": " << pigLatinify(words[i]) << '\n';
+    }
+    return 0;
+}
